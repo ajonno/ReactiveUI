0546b63b10409d917f8319971afabe4d559de626
Angus Johnston <ajonno68@gmail.com>
2016-02-14 13:09:55 +1100

    initial commit

---
+++ ReactiveUI-WPF/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+
+namespace ReactiveUI_WPF
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		// Since we're using the MVVM pattern, we're going to bind to our 
+		// AppViewModel object in the code, and our View code-behind will be
+		// concerned only with things that are solely view-based, like 
+		// minimizing/maximizing a window
+		public AppViewModel AppViewModel { get; private set; }
+
+		public MainWindow()
+		{
+			AppViewModel = new AppViewModel();
+			InitializeComponent();
+		}
+	}
+
+}
